b8cf73d39818709aabf1ba2cd05948d31295693c
brebeca <53571123+brebeca@users.noreply.github.com>
2020-12-09 19:18:24 +0200

    added ml server

---
+++ TSense/ML-Server/Controllers/SentimentSController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.ML;
+using ML_Server.DataModels;
+
+namespace ML_Server.Controllers
+{
+    [Route("api/v1/predictions")]
+    [ApiController]
+    public class SentimentSController : ControllerBase
+    {
+        public PredictionEnginePool<SentimentData, SentimentPrediction> PredictionEnginePool { get; }
+        public SentimentSController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool)
+        {
+            PredictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] SentimentData data)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            SentimentPrediction predictedValue = PredictionEnginePool.Predict(modelName: "SentimentAnalisys", example: data);
+            string sentiment = Convert.ToBoolean(predictedValue.Prediction) ? "Positive" : "Negative";
+            return Ok(sentiment);
+        }
+       
+    }
+}
---
+++ TSense/ML-Server/DataModels/SentimentPrediction.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.ML.Data;
+
+namespace ML_Server.DataModels
+{
+    public class SentimentPrediction :SentimentData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
